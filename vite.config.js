2b3101fe7c9918822746ca06d62b4452865802ea
Freddy L <neo-ciber_94@hotmail.com>
2022-12-04 18:07:04 -0400

    chore: Updated tests to ignore which depends on database on github actions

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from "vitest/config";
+import path from "path";
+
+const isRunningOnGithubAction = process.env.GITHUB_ACTIONS === "true";
+const testToExclude = isRunningOnGithubAction
+  ? ["**/tests/integration_tests/services/extract_transform_json_2.test.ts"]
+  : [];
 
 export default defineConfig({
   test: {
     dir: "./tests/",
+    exclude: [...testToExclude],
   },
 });
